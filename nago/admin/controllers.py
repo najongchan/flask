56883bb867632d4d4a6c39de9c52c76f1e1db41e
maruoov <zmaru123@gmail.com>
2018-06-10 13:39:02 +0900

    add admin

--- nago/admin/controllers.py
+++ nago/admin/controllers.py
@@ -1,7 +1,28 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, session
+from nago.module.Admin import Admin
 
 admin = Blueprint('admin', __name__)
 
+
 @admin.route('/')
 def index():
-    return "Admin"
+    return render_template('/main/adminLogin.html')
+
+
+@admin.route('/login', methods=['POST'])
+def login():
+    dict = request.form.to_dict()
+    admin = Admin()
+
+    result = admin.login(dict)
+
+    if result:
+        print('login success')
+    else:
+        print('login fail')
+
+
+@admin.route('logout')
+def logout():
+    # 로그아웃 처리
+    return True
